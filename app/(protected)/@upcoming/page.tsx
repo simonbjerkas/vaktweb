8667a3de3815020fd524f945298e7ec2104f6d33
simonbjerkas <simon.bjerkas10@gmail.com>
2025-02-06 16:17:24 +0100

    removed unused const

--- app/(protected)/@upcoming/page.tsx
+++ app/(protected)/@upcoming/page.tsx
@@ -2,11 +2,6 @@ import { EventCalendar } from "@/components/event-calendar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const Upcoming = async () => {
-  const upcoming = await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("upcoming");
-    }, 2000);
-  });
   return (
     <Card>
       <CardHeader>
